fd4559d3bcb2499bc77f691531df11b7e5457324
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2023-11-09 10:18:14 -0300

    Treinando WHILE

---
+++ BaseKotlin/src/main/kotlin/fluxo/While.kt
@@ -0,0 +1,31 @@
+package fluxo
+
+fun main() {
+    var count = 0
+    var count2 = 7
+    var count3 = 1
+   while (count < 23){
+        println("Meu contador $count")
+       count++
+   }
+    println("")
+    println("Teste")
+    while (count3 < 23){
+        count3++
+        println("Meu contador teste: $count3")
+        count3++
+        count3++
+
+    }
+    println("")
+    println("possivel")
+
+     while (count2 > 5 && count2 < 15){
+         println("Meu contador e louco: $count2")
+         count2++
+
+
+     }
+
+
+}
